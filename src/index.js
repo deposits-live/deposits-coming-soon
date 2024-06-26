893a1d61b509fbb7022e9be395495ebbc9989a9a
Kartik Bhalla <kartik.bhalla@upgrad.com>
2024-06-27 02:17:54 +0530

    src: push coming soon code
    Signed-off-by: Kartik Bhalla <kartik.bhalla@upgrad.com>

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import Preloader from "./components/Preloader/Preloader";
+import Timer from "./components/Countdown/Timer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./styles.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>
+          Deposits
+          <br />
+          Coming Soon
+        </h1>
+        <Timer />
+        <br />
+        <p>Get ready for all your fixed deposit needs in one place</p>
+        <Preloader />
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
